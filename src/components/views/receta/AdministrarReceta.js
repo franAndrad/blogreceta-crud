bfa6516861e47cc358b99cbd2b7bf7c11e883cd9
franAndrad <franciscoezequielandrade@gmail.com>
2022-09-18 03:47:38 -0300

    se agrego el boton para crear en la pagina de administracion

--- src/components/views/receta/AdministrarReceta.js
+++ src/components/views/receta/AdministrarReceta.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { Table, Container } from 'react-bootstrap';
 import ItemReceta from './ItemReceta';
+import { Link } from 'react-router-dom';
 
 const AdministrarReceta = () => {
 
@@ -21,26 +22,28 @@ const AdministrarReceta = () => {
             <h1 className='display-5 mt-5 text-center'>Administra tus recetas</h1>
             <hr />  
             <Container>
-
-            <Table striped bordered hover responsive>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Titulo</th>
-                        <th>Imagen</th>
-                        <th>Descripcion</th>
-                        <th>Ingredientes</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    listaRecetas.map((receta)=>
-                    <ItemReceta key={receta.id} receta={receta} consultarAPI={consultarAPI}></ItemReceta>
-                    )
-                }
-                </tbody>
-            </Table>
+                <div className='d-flex justify-content-end'>
+                    <Link to={`/administrar/crear`} className="btn btn-primary mb-2">Agregar</Link>
+                </div>
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Titulo</th>
+                            <th>Imagen</th>
+                            <th>Descripcion</th>
+                            <th>Ingredientes</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        listaRecetas.map((receta)=>
+                        <ItemReceta key={receta.id} receta={receta} consultarAPI={consultarAPI}></ItemReceta>
+                        )
+                    }
+                    </tbody>
+                </Table>
             </Container>
 
         </div>

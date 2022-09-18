e70e809f4bf4b83d09ae6a2b1f1156268271e73d
franAndrad <franciscoezequielandrade@gmail.com>
2022-09-18 03:08:15 -0300

    se agrego una consulta get a la api y se simulo un servidor con json server

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import CardReceta from './receta/CardReceta';
 
 const Home = () => {
 
     const [listaRecetas, setListaRecetas] = useState([]);
+    const URL = "http://localhost:3005/recetas";
+
+    useEffect(()=>{
+        consultarAPI();
+    },[]);
+
+    const consultarAPI = async () =>{
+        try {
+            const respuesta = await fetch(URL);
+            const obtenerRecetas = await respuesta.json();
+            setListaRecetas(obtenerRecetas);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div>
@@ -11,9 +26,9 @@ const Home = () => {
           <hr />  
           <div className='row mx-2'>
             {
-            listaRecetas.map((receta) => {
-                <CardReceta key={receta.id} receta={receta}></CardReceta>
-            })
+            listaRecetas.map((receta) =>
+            <CardReceta key={receta.id} receta={receta}></CardReceta>
+            )
             }
           </div>
         </div>
--- src/components/views/receta/CardReceta.js
+++ src/components/views/receta/CardReceta.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
-const CardReceta = () => {
+const CardReceta = (props) => {
     return (
         <div className='col-4 card-group mt-4'>
             <Card>
-                <Card.Img variant="top" src="https://www.pexels.com/es-es/foto/tacos-con-lima-461198/" />
+                <Card.Img variant="top" src={props.receta.imagen}/>
                 <Card.Body>
-                    <Card.Title>Chocotorta</Card.Title>
-                    <Card.Text>
-                        2 Queso crema 250g
-                        3 Paquetes Galleta Chocolina
-                    </Card.Text>
+                    <Card.Title>{props.receta.titulo}</Card.Title>
+                    <Card.Text>{props.receta.descripcion}</Card.Text>
                     <Button variant="primary">Ver mas</Button>
                 </Card.Body>
             </Card>

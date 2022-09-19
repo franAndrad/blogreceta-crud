cbb3f388119095b51a4a96bb186ac30fef467211
franAndrad <franciscoezequielandrade@gmail.com>
2022-09-19 17:57:49 -0300

    se modifico para que el titulo y la descripcion de la card queden siempre abajo por mas de que no tenga imagen

--- src/components/views/receta/CardReceta.js
+++ src/components/views/receta/CardReceta.js
@@ -10,12 +10,14 @@ const CardReceta = (props) => {
         <div className='col-6 col-md-4 col-lg-3 mt-4'>
             <Card className='h-100'>
                 <Card.Img variant="top" className='rounded' src={props.receta.imagen}/>
-                <Card.Body>
-                    <Card.Title>{props.receta.titulo}</Card.Title>
-                    <Card.Text>{props.receta.descripcion}</Card.Text>
-                </Card.Body>
-                <div className='mx-3'>
-                <Link to={'/detalle/'+props.receta._id} className="btn btn-primary mb-3" >Ver mas</Link>
+                <div className='mb-0 mt-auto'>
+                    <Card.Body>
+                        <Card.Title>{props.receta.titulo}</Card.Title>
+                        <Card.Text>{props.receta.descripcion}</Card.Text>
+                    </Card.Body>
+                    <div className='mx-3'>
+                    <Link to={'/detalle/'+props.receta._id} className="btn btn-primary mb-3" >Ver mas</Link>
+                    </div>
                 </div>
             </Card>
         </div>
